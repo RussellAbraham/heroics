a8d3d94e37d53339ac76715b9f63131bd5124294
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-20 05:47:18 -0800

    Update

---
+++ public/assets.js
@@ -0,0 +1,8 @@
+requirejs.config({
+	baseUrl : '',
+	paths : {  
+		cdn : 'https://assets.codepen.io/1674766',    
+		s3 : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1674766',
+	}
+});
+requirejs([], function(){});
