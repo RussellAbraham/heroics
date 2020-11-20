2f4be86385cee70c01bdd60d69c7db03850e12a2
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-20 12:05:36 -0800

    Update modal.js

--- public/modal.js
+++ public/modal.js
@@ -172,3 +172,31 @@
 	return Views;
 
 })(plugins.Views);
+/*
+plugins.Alert = function(message){
+    return new plugins.Views.Modal({
+        title : "Alert",
+        animate : true,
+        allowCancel : true,
+        content : message
+    }).open();	
+};
+
+plugins.Confirm = function(message) {
+    return new plugins.Views.Modal({
+        title: "Confirm",
+        animate: true,
+        allowCancel: false,
+        content: message
+    }).open();
+};
+
+plugins.Prompt = function(str) {
+    return new plugins.Views.Modal({
+        title : "prompt",
+        animate : true,
+        allowCancel: false,
+        content : '<input class="form-control">'
+    }).open();
+};
+*/
