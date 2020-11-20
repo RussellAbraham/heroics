db491f2e9002ddac0cda31f4a23842467d0556d2
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-20 06:06:17 -0800

    Update

--- public/assets.js
+++ public/assets.js
@@ -1,8 +1,16 @@
-requirejs.config({
-	baseUrl : '',
-	paths : {  
-		cdn : 'https://assets.codepen.io/1674766',    
-		s3 : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1674766',
-	}
-});
-requirejs([], function(){});
+(function(global, plugins){
+	requirejs.config({
+		baseUrl : '',
+		paths : {  
+			cdn : 'https://assets.codepen.io/1674766',
+			jquery : 'https://assets.codepen.io/1674766/jquery.min',
+			bootstrap : 'https://assets.codepen.io/1674766/util.min',
+			s3 : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1674766',
+		}
+	});
+	requirejs([ 'jquery', 'bootstrap' ], function($, bootstrap){
+		if(typeof global !== 'undefined'){
+			global.plugins = plugins;
+		}
+	});
+})(this, {});
