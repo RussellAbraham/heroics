e95c85bf9eab033701d19a11f43e1b295b6015c2
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-20 12:00:54 -0800

    Update
    Backbone, Plugins Object, Modal Dialogue

--- public/assets.js
+++ public/assets.js
@@ -1,35 +1,38 @@
-(function(global, plugins){
+(function (global) {
+
+	const plugins = {
+		Models: {},
+		Collections: {},
+		Views: {},
+		Routes: {}
+	};
+
 	requirejs.config({
-		baseUrl : '',
-		paths : {  
-			cdn : 'https://assets.codepen.io/1674766',
-			jquery : 'https://assets.codepen.io/1674766/jquery.min',
-			bootstrap : 'https://assets.codepen.io/1674766/util.min',
-			s3 : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1674766',
+		baseUrl: '',
+		shim: {
+			underscore: {
+				exports: '_'
+			},
+			backbone: {
+				deps: ['underscore', 'jquery'],
+				exports: 'Backbone'
+			}
+		},
+		paths: {
+			cdn: 'https://assets.codepen.io/1674766',
+			jquery: 'https://assets.codepen.io/1674766/jquery.min',
+			underscore: 'https://assets.codepen.io/1674766/underscore.min',
+			backbone: 'https://assets.codepen.io/1674766/backbone.min',
+			bootstrap: 'https://assets.codepen.io/1674766/util.min',
+			terminal: 'https://assets.codepen.io/1674766/jquery.terminal.min',
+			s3: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1674766',
 		}
 	});
-	requirejs([ 'jquery', 'bootstrap' ], function($, bootstrap){
-		function ProtocolHandler(scheme, url, title){
-			this.scheme = scheme;
-			this.url = url;
-			this.title = title;
-		}
-		ProtocolHandler.prototype = {
-			register : function(){
-				navigator.registerProtocolHandler( this.scheme, this.url, this.title );
-			},
-			unregister : function(){
-				navigator.unregisterProtocolHandler( this.scheme, this.title );
-			},
-			check : function(){
-				if('isProtocolHandlerRegistered' in navigator){
-					alert(navigator.isProtocolHandlerRegistered( this.scheme, this.url ));
-				}
-			}
-		};
-		plugins.protocolHandler = new ProtocolHandler('web+echo','https://heroics.herokuapp.com/?echo=%s','Heroku Handler');
-		if(typeof global !== 'undefined'){
+
+	requirejs(['backbone'], function (Backbone) {
+		if (typeof global !== 'undefined') {
 			global.plugins = plugins;
 		}
 	});
-})(this, {});
+
+})(this);
