284ee84ec3aa5a3e75f580b81ccea4fc5acec830
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-21 23:33:23 -0800

    Update

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
+
+const cool = require('cool-ascii-faces');
+
+const {
+  Pool
+} = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,16 +20,35 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('home'))
-  .get('/about', function(req, res){ res.render('about') })
-  .get('/contact', function(req, res){ res.render('contact') })
+  .get('/about', function (req, res) {
+    res.render('about')
+  })
+  .get('/contact', function (req, res) {
+    res.render('contact')
+  })
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  showTimes = () => {
-    let result = '';
-    const times = process.env.TIMES || 5;
-    for (i = 0; i < times; i++) {
-      result += i + ' ';
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = {
+        'results': (result) ? result.rows : null
+      };
+      res.render('db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
     }
-    return result;
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
   }
+  return result;
+}
